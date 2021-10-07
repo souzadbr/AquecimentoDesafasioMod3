938962aa82bc9f6ca2dfa5c3834452765711cff4
Debora Souza <deboral.p@hotmail.com>
2021-10-07 15:47:55 -0300

    Criar metodo cosntrutor na classe Consumidor

--- src/br/com/zup/Consumidor.java
+++ src/br/com/zup/Consumidor.java
@@ -5,5 +5,9 @@ public class Consumidor {
     private String email;
     private TipoConsumidor tipo;
 
-
+    public Consumidor(String nome, String email, TipoConsumidor tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.tipo = tipo;
+    }
 }
