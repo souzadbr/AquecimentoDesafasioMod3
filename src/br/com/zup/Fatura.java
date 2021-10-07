570e445d991f718f9b03a4839c06e216ff91c510
Debora Souza <deboral.p@hotmail.com>
2021-10-07 15:39:48 -0300

    Adição metodo construtor na classe Fatura com todos atributos

--- src/br/com/zup/Fatura.java
+++ src/br/com/zup/Fatura.java
@@ -4,4 +4,10 @@ public class Fatura {
     private double valor;
     String dataDeVencimento;
     Consumidor consumidor;
+
+    public Fatura(double valor, String dataDeVencimento, Consumidor consumidor) {
+        this.valor = valor;
+        this.dataDeVencimento = dataDeVencimento;
+        this.consumidor = consumidor;
+    }
 }
